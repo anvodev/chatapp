6334103d8457c44e5f22b920020a694603ce79cb
An Vo <votanlean@gmail.com>
2024-04-07 08:31:59 +0700

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module chatapp.anvo.dev
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	setupAPI()
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func setupAPI() {
+	http.Handle("/", http.FileServer(http.Dir("public")))
+}
